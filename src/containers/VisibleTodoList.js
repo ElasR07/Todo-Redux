f0789e1f5f29c58a9e63791b1c35103ef4921257
Victor Requena <svictoreq@gmail.com>
2018-10-21 22:59:52 -0400

    Fix unknown filter error

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -1,12 +1,12 @@
 import {
-    connect
+    connect,
 } from 'react-redux';
 import {
-    toggleTodo
+    toggleTodo,
 } from '../actions';
 import TodoList from '../components/TodoList';
 import {
-    visibilityFilters
+    visibilityFilters,
 } from '../actions';
 
 const getVisibleTodos = (todos, filter) => {
@@ -14,20 +14,20 @@ const getVisibleTodos = (todos, filter) => {
         case visibilityFilters.SHOW_ALL:
             return todos;
         case visibilityFilters.SHOW_COMPLETED:
-            return todos.filter(t => t.completed)
+            return todos.filter(t => t.completed);
         case visibilityFilters.SHOW_ACTIVE:
-            return todos.filter(t => !t.completed)
+            return todos.filter(t => !t.completed);
         default:
-            throw new Error('Unknown filter: ' + filter)
+            throw new Error('Unknown filter: ' + filter);
     }
 }
 
 const mapStateToProps = state => ({
-    todos: getVisibleTodos(state.todos, state.visibilityFilter)
+    todos: getVisibleTodos(state.todos, state.visibilityFilter),
 })
 
 const mapDispatchToProps = dispatch => ({
-    toggleTodo: id => dispatch(toggleTodo(id))
+    toggleTodo: id => dispatch(toggleTodo(id)),
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(getVisibleTodos)
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
